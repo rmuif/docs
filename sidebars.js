544bcf959cf0a0cd9a1c424c707d049432959567
Linus Långberg <git@phoqe.dev>
2020-03-07 17:30:11 +0100

    Fix sidebar nodes

--- sidebars.js
+++ sidebars.js
@@ -1,13 +1,26 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 module.exports = {
-  someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['mdx'],
-  },
+  docs: [
+    {
+      type: "doc",
+      id: "getting-started"
+    },
+    {
+      type: "doc",
+      id: "scripts"
+    },
+    {
+      type: "category",
+      label: "Configuration",
+      items: ["firebase", "sentry"]
+    },
+    {
+      type: "category",
+      label: "Maintenance",
+      items: ["authentication", "authorization", "pages", "updates"]
+    },
+    {
+      type: "doc",
+      id: "roadmap"
+    }
+  ]
 };
